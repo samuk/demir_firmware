2e1a46f9d6347e8de07f4005a9d295fbf00307bb
samuk <sam@bristolwireless.net>
2025-10-23 13:48:00 +0100

    Add I2C communication functions and buffer
    Implement I2C communication interface for DEMIR controller.

---
+++ src/i2c_comm.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file i2c_comm.cpp
+ * @brief I2C communication interface implementation for DEMIR controller
+ * @author Muhammet Şükrü Demir
+ * @date 2025
+ */
+
+#include "i2c_comm.h"
+
+uint8_t i2cCmdBuffer[I2C_CMD_BUF_SIZE];
+uint8_t i2cCmdIndex = 0;
+
+void receiveI2CCommand(int byteCount) {
+    while (Wire.available() && i2cCmdIndex < I2C_CMD_BUF_SIZE) {
+        i2cCmdBuffer[i2cCmdIndex++] = Wire.read();
+    }
+    if (i2cCmdIndex > 0) {
+        Solver.proccessI2CCommand(i2cCmdBuffer, i2cCmdIndex);
+        i2cCmdIndex = 0;
+    }
+}
+
+void sendI2CResponse() {
+    uint32_t pos = Motor.getPosition(MOTOR_1);
+    Wire.write((uint8_t*)&pos, sizeof(pos));
+}
